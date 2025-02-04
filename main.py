406fce63a08ee4be102274c2b714ce3a4a8a9d45
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-04 20:46:04 +0200

    Tasks add

--- main.py
+++ main.py
@@ -13,3 +13,31 @@ house1.country = ("LV")
 
 print(house1.country, house1.price, house1.year)
 print(house2.country, house2.price, house2.year)
+print('-------------------------------')
+# Example
+house_instance_1 = House(10_000, 1990)
+house_instance_2 = House(12_000, 1920)
+print(house_instance_1)
+print(house_instance_2)
+print('-------------------------------')
+# Example
+from datetime import datetime
+
+class House:
+    country = "LT"
+
+    def __init__(self, price, year):
+        self.price = price
+        self.year = year
+
+    def get_age(self):
+        now = datetime.today()
+        current_year = now.year
+        return current_year - self.year
+
+house_instance_1 = House(10_000, 1990)
+age = house_instance_1.get_age()
+print(age)
+
+house_instance_2 = House(12_000, 1900)
+print(house_instance_2.get_age())
--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 # Task 1
 class Car:
     manufacturer = 'Toyota'
@@ -5,6 +6,7 @@ print(Car.manufacturer)
 class Bike:
     manufacturer = 'Yamaha'
 print(Bike.manufacturer)
+print('-------------------------------')
 # Task 2
 class Book:
     publisher = "Penquin"
@@ -17,5 +19,27 @@ class Book:
 book1 = Book('The Little Prince',  ' Antoine de Saint-Exupéry', '1943')
 book2 = Book('Harry Potter',  ' J. K. Rowling', '1997')
 print(Book.publisher)
-print(f'{book1.title} is written by:  {book1.author}')
-print(f'{book2.title} is written by:  {book2.author}')
+print(f'{book1.title} is written by: {book1.author}')
+print(f'{book2.title} is written by: {book2.author}')
+print('-------------------------------')
+# Task 3
+print(f'Book instantion: {book1}')
+print('-------------------------------')
+# Task 4-5
+class Book:
+    publisher = "Penquin"
+
+    def __init__(self, title, author ,year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_age(self):
+        now = datetime.today()
+        current_year = now.year
+        return current_year - self.year
+
+book1 = Book('The Little Prince',  ' Antoine de Saint-Exupéry', 1943)
+book2 = Book('Harry Potter',  ' J. K. Rowling', 1997)
+print(f'{book1.title} is {book1.get_age()} years old.')
+print(f'{book2.title} is {book2.get_age()} years old.')
